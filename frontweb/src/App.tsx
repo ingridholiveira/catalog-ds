3f19f3c8ac7b05d6e06f993d6c3dc1fa2ef04810
Ingridh Oliveira <ingridhnunes@gmail.com>
2022-05-18 21:42:35 -0300

    Navbar

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,12 +1,14 @@
+import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
 
 const App = () => {
   return (
     <>
-    <h1>Olá DS-catalog!</h1>
+      <Navbar />
+      <h1>Olá DS-catalog!</h1>
     </>
   );
-}
+};
 
 export default App;
