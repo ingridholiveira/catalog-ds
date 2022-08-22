f326b66cf77850f794b5125bee67e39df36c5ba6
Ingridh Oliveira <ingridhnunes@gmail.com>
2022-08-22 13:59:53 -0300

    routes

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,11 +1,13 @@
-import Home from 'pages/Home';
+//import Home from 'pages/Home';
+
 import './assets/styles/custom.scss';
 import './App.css';
+import Routes from 'Routes';
 
 const App = () => {
   return (
     <>
-      <Home />
+      <Routes />
     </>
   );
 };
